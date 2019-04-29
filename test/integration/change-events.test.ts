ca2f7775d2887a6e705eb4454872d98c1c7079ec
Michiel de Jong <michiel@unhosted.org>
2019-04-29 09:49:21 +0200

    add first integration test

---
+++ test/integration/change-events.test.ts
@@ -0,0 +1,37 @@
+import { Server } from '../../src/server'
+import fetch from 'node-fetch'
+import WebSocket from 'ws'
+
+let wsClient: WebSocket
+let server: Server
+let received: Promise<string>
+
+beforeEach(async () => {
+  server = new Server(8080, 'http://localhost:8080')
+  server.listen()
+  wsClient = new WebSocket('ws://localhost:8080')
+  received = new Promise((resolve) => {
+    wsClient.on('message', function incoming (data) {
+      resolve(data.toString())
+    })
+  })
+  await new Promise((resolve) => {
+    wsClient.on('open', function open () {
+      wsClient.send('sub root/asdf/')
+      resolve(undefined)
+    })
+  })
+})
+afterEach(() => {
+  wsClient.close()
+  server.close()
+})
+
+test('publishes a change event', async () => {
+  await fetch('http://localhost:8080/asdf/test.txt', {
+    method: 'PUT',
+    body:  'hello'
+  })
+  const notif = await received
+  expect(notif).toEqual('pub root/asdf/test.txt')
+})
