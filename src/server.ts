d7c536566ab092531cc4159ec3b0686af5bcb10c
Michiel de Jong <michiel@unhosted.org>
2019-09-20 12:15:50 +0200

    use wac-ldp@0.9.1

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import * as http from 'http'
 import Debug from 'debug'
-import { BlobTreeInMem, BlobTree, WacLdp, QuadAndBlobStore, NssCompatResourceStore, DefaultOperationFactory, AclBasedAuthorizer } from 'wac-ldp'
+import { BlobTreeInMem, BlobTree, WacLdp, QuadAndBlobStore, NssCompatResourceStore, DefaultOperationFactory, AclBasedAuthorizer, StoreManager } from 'wac-ldp'
 import * as WebSocket from 'ws'
 import { Hub } from './lib/hub'
+import IResourceStore from 'solid-server-ts/src/ldp/IResourceStore'
 
 const debug = Debug('server')
+const dataDir = process.env.DATA_DIR || './data'
 
 export class Server {
-  storage: QuadAndBlobStore
   wacLdp: WacLdp
   server: http.Server
   hub: Hub
@@ -16,13 +17,16 @@ export class Server {
   owner: URL | undefined
   constructor (port: number, aud: string, owner: URL | undefined) {
     this.port = port
-    this.storage = new QuadAndBlobStore(new BlobTreeInMem()) // singleton in-memory storage
+    const lowLevelResourceStore: IResourceStore = new NssCompatResourceStore(dataDir)
+    const midLevelResourceStore: IResourceStore = new QuadAndBlobStore(lowLevelResourceStore as BlobTree) // singleton on-disk storage
+    const serverRootDomain: string = new URL(aud).host
+    const highLevelResourceStore: IResourceStore = new StoreManager(serverRootDomain, midLevelResourceStore as QuadAndBlobStore)
+    const operationFactory = new DefaultOperationFactory(highLevelResourceStore)
+    const authorizer = new AclBasedAuthorizer(highLevelResourceStore as StoreManager)
+
     const skipWac = (owner === undefined)
-    const resourceStore = new NssCompatResourceStore()
-    const operationFactory = new DefaultOperationFactory(resourceStore)
-    const authorizer = new AclBasedAuthorizer(resourceStore)
     this.wacLdp = new WacLdp(operationFactory, authorizer, {
-      storage: this.storage,
+      storage: midLevelResourceStore as QuadAndBlobStore,
       aud,
       updatesViaUrl: new URL(`ws://localhost:${this.port}/`),
       skipWac,
